1a961751eb2675a22359aba3e52dc0fb6f0dc653
lucabudi <lucabudi@gmail.com>
2025-10-21 23:01:25 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/gozone' : '',
-  basePath: isProd ? '/gozone' : '',
+  assetPrefix: '/gozone',
+  basePath: '/gozone',
   images: {
     unoptimized: true,
     domains: ['images.unsplash.com', 'i.guim.co.uk'],
