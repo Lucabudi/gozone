dbb0948df6a86e50cb15dd8ecda5e86f7a591061
lucabudi <lucabudi@gmail.com>
2025-10-21 23:06:28 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: '/gozone',
+  assetPrefix: '/gozone/',
   basePath: '/gozone',
   images: {
     unoptimized: true,
