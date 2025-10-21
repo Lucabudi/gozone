0612ae591876029a4f09b449c081d05ec5fb9e6d
lucabudi <lucabudi@gmail.com>
2025-10-21 22:23:28 +0200

    Updates

---
+++ _next/static/_low7TU0l3QYiaWv8Qxnq/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/1a2fae3c2c663123.js"
+  ],
+  "/chisiamo": [
+    "./static/chunks/ad8e734a1e4c2829.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gozone//_next/:path+",
+        "destination": "/gozone/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error",
+    "/chisiamo"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/2f530546d457bd14.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/df6ba8053cfabf26.js",
+  "static/chunks/c17240bdb9f39308.js",
+  "static/chunks/turbopack-bcd18b8bf40a3616.js"
+])
