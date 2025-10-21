63d17131a944f77414ca011c34c9deb84023a6ab
lucabudi <lucabudi@gmail.com>
2025-10-21 22:40:00 +0200

    Updates

--- _next/static/chunks/1a2fae3c2c663123.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/736b4122ba01afbe.js",
-  "static/chunks/c17240bdb9f39308.js",
-  "static/chunks/turbopack-a6d7a0fa24db1639.js"
-])
--- _next/static/chunks/ad8e734a1e4c2829.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/chisiamo", [
-  "static/chunks/b02220abae165934.js",
-  "static/chunks/c17240bdb9f39308.js",
-  "static/chunks/turbopack-9c6ff738f602f03b.js"
-])
