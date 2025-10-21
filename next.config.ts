f134f56e79cae24b9fd05a489ac57f4d602e956b
lucabudi <lucabudi@gmail.com>
2025-10-21 22:03:35 +0200

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */ 
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: '/gozone/', // Replace with your GitHub repo name
+  basePath: '/gozone', // Replace with your GitHub repo name
   images: {
-        domains: ['images.unsplash.com', 'i.guim.co.uk'],
-    },
+    unoptimized: true,
+    domains: ['images.unsplash.com', 'i.guim.co.uk'],
+  },
 };
 
 export default nextConfig;
